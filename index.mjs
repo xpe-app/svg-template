6677da43cdf21564ea3a4bce0dfe19cfd9719a7b
Colin Leung <colin@cowise.co>
2024-07-14 01:12:44 +0800

    update export

--- index.mjs
+++ index.mjs
@@ -1,5 +1,9 @@
 import SVGTemplate  from "./SVGTemplate.mjs";
+import Utils from "./parser/Utils.mjs";
 
-export default {
-  SVGTemplate
+export default SVGTemplate;
+
+export{
+  SVGTemplate,
+  Utils
 }
